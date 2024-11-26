b3eb8725f51380420eaa433459c468730cd1f48e
adiarhmn <adiauliarahmn@gmail.com>
2024-11-26 09:32:37 +0800

    Update Admin Layout

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,6 +1,23 @@
 @extends('layout.admin_layout')
 @section('content')
-    <div>
-        Dashboard
-    </div>
+    <section class="ps-3">
+
+        {{-- Header Page --}}
+        <div class="row">
+            <h4 class="p-0">Dashboard</h4>
+            <nav class="page-breadcrumb p-0">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="{{ url('admin/dashboard') }}">Dashboard</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Analytics</li>
+                </ol>
+            </nav>
+        </div>
+
+        {{-- Main --}}
+        <div>
+            <div class="row">
+                Selamat Datang Bos
+            </div>
+        </div>
+    </section>
 @endsection
--- resources/views/layout/admin_layout.blade.php
+++ resources/views/layout/admin_layout.blade.php
@@ -1,40 +1,194 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Galyna Heiwa</title>
-
-
-    {{-- CSS --}}
-    <link rel="stylesheet" href="{{ asset('assets/vendors/core/core.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/fonts/feather-font/css/iconfont.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/vendors/flag-icon-css/css/flag-icon.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/style.min.css') }}">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Modernize Free</title>
+    <link rel="stylesheet" href="{{ url('/') }}/assets-modernize/css/styles.min.css" />
+    <link rel="stylesheet" href="{{ url('/') }}/assets/css/admin_layout.css" />
 </head>
 
 <body>
-    <div class="main-wrapper">
+    <!--  Body Wrapper -->
+    <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+        data-sidebar-position="fixed" data-header-position="fixed">
+        <!-- Sidebar Start -->
+        <aside class="left-sidebar">
+            <!-- Sidebar scroll-->
+            <div>
+                <div class="brand-logo d-flex align-items-center justify-content-between">
+                    <a href="./index.html" class="text-nowrap logo-img">
+                        <img src="{{ url('/') }}/assets/galyna/logo-v2-transparent.svg" width="200"
+                            alt="Galyna Logo" />
+                    </a>
+                    <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
+                        <i class="ti ti-x fs-8"></i>
+                    </div>
+                </div>
+                <!-- Sidebar navigation-->
+                <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
+                    <ul id="sidebarnav">
+                        <li class="nav-small-cap">
+                            <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+                            <span class="hide-menu">Home</span>
+                        </li>
 
-        @include('layout.partials._sidebar')
+                        {{-- Dashboard Menu --}}
+                        <li class="sidebar-item">
+                            <a class="sidebar-link {{ Request::is('admin/dashboard') ? 'active' : '' }}"
+                                href="{{ url('/admin/dashboard') }}">
 
-        <div class="page-wrapper">
-            @include('layout.partials._navbar')
+                                <span>
+                                    <i class="ti ti-layout-dashboard"></i>
+                                </span>
+                                <span class="hide-menu">Dashboard</span>
+                            </a>
+                        </li>
+                        <li class="nav-small-cap">
+                            <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+                            <span class="hide-menu">Data Master</span>
+                        </li>
 
-            <div class="page-content">
+                        {{-- Category Menu --}}
+                        <li class="sidebar-item">
+                            <a class="sidebar-link {{ Request::is('admin/category') ? 'active' : '' }}"
+                                href="{{ url('/admin/category') }}" aria-expanded="false">
+                                <span>
+                                    <i class="ti ti-category-2"></i>
+                                </span>
+                                <span class="hide-menu">Category</span>
+                            </a>
+                        </li>
+
+                        {{-- Product Menu --}}
+                        <li class="sidebar-item">
+                            <a class="sidebar-link {{ Request::is('admin/products') ? 'active' : '' }}"
+                                href="{{ url('/admin/products') }}" aria-expanded="false">
+                                <span>
+                                    <i class="ti ti-shirt"></i>
+                                </span>
+                                <span class="hide-menu">Products</span>
+                            </a>
+                        </li>
+
+                        {{-- Product Menu --}}
+                        <li class="sidebar-item">
+                            <a class="sidebar-link {{ Request::is('admin/article') ? 'active' : '' }}"
+                                href="{{ url('/admin/article') }}" aria-expanded="false">
+                                <span>
+                                    <i class="ti ti-file-description"></i>
+                                </span>
+                                <span class="hide-menu">Article</span>
+                            </a>
+                        </li>
+
+                        {{-- Product Menu --}}
+                        <li class="sidebar-item">
+                            <a class="sidebar-link {{ Request::is('admin/links') ? 'active' : '' }}"
+                                href="{{ url('/admin/links') }}" aria-expanded="false">
+                                <span>
+                                    <i class="ti ti-link"></i>
+                                </span>
+                                <span class="hide-menu">Links</span>
+                            </a>
+                        </li>
+                        <li class="nav-small-cap">
+                            <span class="hide-menu">COMPANY</span>
+                        </li>
+                        <li class="sidebar-item">
+                            <a class="sidebar-link" href="./authentication-login.html" aria-expanded="false">
+                                <span>
+                                    <i class="ti ti-login"></i>
+                                </span>
+                                <span class="hide-menu">Login</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a class="sidebar-link" href="./authentication-register.html" aria-expanded="false">
+                                <span>
+                                    <i class="ti ti-user-plus"></i>
+                                </span>
+                                <span class="hide-menu">Register</span>
+                            </a>
+                        </li>
+                        <li class="nav-small-cap">
+                            <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+                            <span class="hide-menu">EXTRA</span>
+                        </li>
+                        <li class="sidebar-item">
+                            <a class="sidebar-link" href="./icon-tabler.html" aria-expanded="false">
+                                <span>
+                                    <i class="ti ti-mood-happy"></i>
+                                </span>
+                                <span class="hide-menu">Icons</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a class="sidebar-link" href="./sample-page.html" aria-expanded="false">
+                                <span>
+                                    <i class="ti ti-aperture"></i>
+                                </span>
+                                <span class="hide-menu">Sample Page</span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <!-- End Sidebar navigation -->
+            </div>
+            <!-- End Sidebar scroll-->
+        </aside>
+        <!--  Sidebar End -->
+
+        <!--  Main wrapper -->
+        <div class="body-wrapper">
+            <!--  Header Start -->
+            <header class="app-header">
+                <nav class="navbar navbar-expand-lg navbar-light">
+                    <ul class="navbar-nav">
+                        <li class="nav-item d-block d-xl-none">
+                            <a class="nav-link sidebartoggler nav-icon-hover" id="headerCollapse"
+                                href="#">
+                                <i class="ti ti-menu-2"></i>
+                            </a>
+                        </li>
+                    </ul>
+                    <div class="navbar-collapse justify-content-end px-0" id="navbarNav">
+                        <ul class="navbar-nav flex-row ms-auto align-items-center justify-content-end">
+                            <li class="nav-item dropdown">
+                                <a class="nav-link nav-icon-hover" href="javascript:void(0)" id="drop2"
+                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                    <img src="{{ url('/') }}/assets-modernize/images/user.png" alt=""
+                                        width="35" height="35" class="User Profile">
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-end dropdown-menu-animate-up"
+                                    aria-labelledby="drop2">
+                                    <div class="message-body">
+                                        <a href="#" class="d-flex align-items-center gap-2 dropdown-item">
+                                            <i class="ti ti-user fs-6"></i>
+                                            <p class="mb-0 fs-3">My Profile</p>
+                                        </a>
+                                        <a href="./authentication-login.html"
+                                            class="btn btn-outline-danger mx-3 mt-2 d-block">Logout</a>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </header>
+            <!--  Header End -->
+            <div class="container-fluid">
                 @yield('content')
             </div>
-
-            @include('layout.partials._footer')
         </div>
     </div>
-
-    <script src="{{ asset('assets/vendors/core/core.js') }}"></script>
-    <script src="{{ asset('assets/vendors/feather-icons/feather.min.js') }}"></script>
-    <script src="{{ asset('assets/js/template.js') }}"></script>
-
+    <script src="{{ url('/') }}/assets-modernize/libs/jquery/dist/jquery.min.js"></script>
+    <script src="{{ url('/') }}/assets-modernize/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ url('/') }}/assets-modernize/js/sidebarmenu.js"></script>
+    <script src="{{ url('/') }}/assets-modernize/js/app.min.js"></script>
+    {{-- <script src="{{ url('/') }}/assets-modernize/libs/apexcharts/dist/apexcharts.min.js"></script> --}}
+    <script src="{{ url('/') }}/assets-modernize/libs/simplebar/dist/simplebar.js"></script>
 </body>
 
 </html>
