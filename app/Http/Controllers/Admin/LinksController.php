a3edd1156be38cfa6912173a721059c671be3e29
fpendii <144331827+fpendii@users.noreply.github.com>
2024-11-28 10:51:34 +0800

    update fitur add link

--- app/Http/Controllers/Admin/LinksController.php
+++ app/Http/Controllers/Admin/LinksController.php
@@ -13,7 +13,40 @@ class LinksController extends Controller
         // Get all category data
         $links = LinksModel::all();
 
+
         // Return back with data
         return view('admin.links.data', compact('links'));
+    }
+
+    public function create()
+    {
+        // Data for form
+        $data = [
+            'form' => 'Create',
+            'action' => url('admin/links')
+        ];
+
+        // Return view with data
+        return view('admin.links.form', compact('data'));
+    }
+
+    public function store(Request $request)
+    {
+        // Validate Input
+        $request->validate([
+            'name' => 'required|unique:category,name|max:30',
+            'link' => ['required', 'regex:/^https?:\/\/[^\s]+$/', 'max:255']
+        ]);
+
+
+        // Save Data
+        $category = new LinksModel();
+        $category->name = $request->name;
+        $category->link = $request->link;
+        $category->save();
+
+        // Return back with success message
+        session()->flash('success', 'Data saved successfully');
+        return redirect('admin/links');
     }
 }
